8798073dde6906c9c1d783972ae97745e2cb82b0
masta <mastanilewicz@gmail.com>
2022-01-29 16:06:46 +0000

    user profile screen, update functionality, fetching userDetails, and displing them in form

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -95,6 +98,47 @@ export const register = (name, email, password) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+// GET USER DETAILS
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    })
+
+    // Get userInfo from state
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    // prepare headers with user token inside
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        // token
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    // get data from specific user based on passed id
+    const { data } = await axios.get(`/api/users/${id}`, config)
+
+    // pass data to reducer
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,94 @@
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col, FormGroup, FormLabel, FormControl } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { getUserDetails } from '../actions/userActions.js'
+
+const ProfileScreen = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  // get userDetails state from store
+  const userDetails = useSelector(state => state.userDetails)
+  const { loading, error, user } = userDetails
+
+  // get userLogin state from store to check who is logged in
+  const userLogin = useSelector(state => state.userLogin)
+  const {userInfo} = userLogin
+
+  // Check for logged in user / get details /  and fill up form fields
+  useEffect(() => {
+    // if there is no logged in user then navigate to log in screen
+    if(!userInfo) {
+      navigate('/login')
+    } else {
+      if(!user.name) {
+        // get user details
+        dispatch(getUserDetails('profile'))
+      } else {
+        // fill fields with fetched user details
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, userInfo])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    // validate new password
+    if(password !== confirmPassword) {
+      setMessage('Password do not match')
+    } else {
+      
+    }
+  }
+
+  return <Row>
+    <Col md={6}>
+    <h2>User Profile</h2>
+      {error && <Message variant='danger'>{error}</Message>}
+      {message && <Message variant='danger'>{message}</Message>}
+      {loading ? <Loader /> : (       
+        <Form onSubmit={submitHandler}>
+        
+        <FormGroup className='my-3' controlId='name'>
+          <FormLabel>Username</FormLabel>
+          <FormControl type='text' placeholder='Update username' value={name} onChange={(e)=> setName(e.target.value)}></FormControl>
+        </FormGroup>
+
+        <FormGroup className='my-3' controlId='email'>
+          <FormLabel>Email Address</FormLabel>
+         <FormControl type='email' placeholder='Update email' value={email} onChange={(e) => setEmail(e.target.value)}></FormControl>
+        </FormGroup>
+
+        <FormGroup className='my-3' controlId='password'>
+          <FormLabel>Password</FormLabel>
+          <FormControl type='password' placeholder='New password' value={password} onChange={(e) => setPassword(e.target.value)}></FormControl>
+        </FormGroup>
+
+        <FormGroup className='my-3' controlId='ConfirmPassword'>
+          <FormLabel>Confiorm password</FormLabel>
+          <FormControl type='password' placeholder='Confirm new password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></FormControl>
+        </FormGroup>
+
+        <Button className='my-3' type='submit' variant='primary'>Update Details</Button>
+
+        </Form>
+      )}
+    </Col>
+    <Col md={6}>
+      <h2>My Orders</h2>
+    </Col>
+  </Row>;
+};
+
+export default ProfileScreen;

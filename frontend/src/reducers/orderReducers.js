467e44a5b5f06ac57cb2b8fe1b4d5b4a7662c393
masta <mastanilewicz@gmail.com>
2022-02-03 10:22:41 +0000

    getOrderDetails action and reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -3,6 +3,9 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_RESET,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants.js'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -13,8 +16,24 @@ export const orderCreateReducer = (state = {}, action) => {
       return { loading: false, success: true, order: action.payload }
     case ORDER_CREATE_FAIL:
       return { loading: false, error: action.payload }
-    case ORDER_CREATE_RESET:
-      return { loading: false }
+    // case ORDER_CREATE_RESET:
+    //   return { loading: false }
+    default:
+      return state
+  }
+}
+
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddress: {} },
+  action
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return { ...state, loading: true }
+    case ORDER_DETAILS_SUCCESS:
+      return { ...state, loading: false, order: action.payload }
+    case ORDER_DETAILS_FAIL:
+      return { ...state, loading: false, error: action.payload }
     default:
       return state
   }

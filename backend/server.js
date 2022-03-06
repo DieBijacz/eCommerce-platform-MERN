f1d3e706c21091c920ebce125e36d17fdcc2a253
masta <mastanilewicz@gmail.com>
2022-03-06 17:59:05 +0000

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.get('/api/config/paypal', (req, res) =>
 // app is an instance of express.
 // METHOD is an HTTP request method, in lowercase.
 // PATH is a path on the server.
-// HANDLER is the function executed when the route is matched.
+// HANDLER is the function executed when the route is matched
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
